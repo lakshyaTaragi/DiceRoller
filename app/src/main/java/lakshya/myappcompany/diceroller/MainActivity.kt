94569e57df7c420f17e011369d497019a39d2be2
Lakshya Taragi <lakshya.taragi01@gmail.com>
2020-10-08 02:31:53 +0530

    HW: clear button added (used visibility instead of empty_image)

--- app/src/main/java/lakshya/myappcompany/diceroller/MainActivity.kt
+++ app/src/main/java/lakshya/myappcompany/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package lakshya.myappcompany.diceroller
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -20,16 +21,28 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener{ clearImages() }
     }
 
     private fun rollDice() {
 
         diceImage1 = findViewById(R.id.dice_image1)
         diceImage2 = findViewById(R.id.dice_image2)
+        diceImage1.visibility = View.VISIBLE
+        diceImage2.visibility = View.VISIBLE
 
         getRandomDiceImage(diceImage1, (1..6).random())
         getRandomDiceImage(diceImage2, (1..6).random())
 
+    }
+
+    private fun clearImages() {
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage1.visibility = View.INVISIBLE
+        diceImage2.visibility = View.INVISIBLE
     }
 
     private fun getRandomDiceImage(diceImage: ImageView, randomInt: Int) {

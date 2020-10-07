faa949cd8a921e70e3a67064b8a9eff3c88a41f9
Lakshya Taragi <lakshya.taragi01@gmail.com>
2020-10-08 02:11:49 +0530

    Added backward compatibility for vector file resources + code challenge done

--- app/src/main/java/lakshya/myappcompany/diceroller/MainActivity.kt
+++ app/src/main/java/lakshya/myappcompany/diceroller/MainActivity.kt
@@ -1,43 +1,48 @@
 package lakshya.myappcompany.diceroller
 
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_up_button)
         rollButton.setOnClickListener { rollDice() }
-//        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-//        resultText.text = "Dice Rolled!"
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+        getRandomDiceImage(diceImage1, (1..6).random())
+        getRandomDiceImage(diceImage2, (1..6).random())
+
     }
 
-/*
-private fun countUp() {
-val resultText: TextView = findViewById(R.id.result_text)
-val currentVal = resultText.text.toString()
-var finalVal= 0
-when (currentVal) {
-"Hello, World!" -> finalVal=1
-"6" -> finalVal=6
-else -> finalVal = currentVal.toInt()+1
-}
+    private fun getRandomDiceImage(diceImage: ImageView, randomInt: Int) {
+        val drawableResource = when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
 
 
-resultText.text = (finalVal).toString()
-}
-*/
 }
